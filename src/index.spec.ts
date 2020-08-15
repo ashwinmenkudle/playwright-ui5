daea3fd7a465643f1929e25202056511e351e10d
Ashwin Menkudle <Ashwin.Menkudle@sas.com>
2020-08-15 10:49:56 -0400

    Adding UI5 test for component without property

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,27 +1,33 @@
-import { chromium } from "playwright";
+import { chromium, ChromiumBrowser, Page } from "playwright";
 import { expect } from "chai"
 import { Server } from 'node-static';
 import * as http from "http"
 import { register, click, isPresent } from ".";
 
 
-describe('First test', () => {
+describe('Select UI5 component by property', () => {
     let server: http.Server
-    beforeEach(() => {
+    let page: Page
+    let browser: ChromiumBrowser
+
+    beforeAll( async () => {
         var file = new Server('./fixtures');
         server = http.createServer(function (request, response) {
             request.addListener('end', function () {
                 file.serve(request, response);
             }).resume();
         }).listen(8080);
+        await register()
+      });
+
+    beforeEach(async () => {
+        // const browser = await chromium.launch({ headless: false, devtools: true })
+        browser = await chromium.launch()
+        let context = await browser.newContext()
+        page = await context.newPage();
     })
 
-    test('should return true', async () => {
-        await register()
-        // const browser = await chromium.launch({ headless: false, devtools: true })
-        const browser = await chromium.launch()
-        let context = await browser.newContext()
-        const page = await context.newPage();
+    test('select by one propery', async () => {
         await page.goto('http://localhost:8080/property-select/index.html');
 
         await click(page, 'sap.m.Button[text="Test"]')
@@ -29,16 +35,28 @@ describe('First test', () => {
 
         let button = await page.$('tag=button');
         expect(button).to.be.a("object")
-
-        browser.close()
     });
 
+    test('select by component', async () => {
+        await page.goto('http://localhost:8080/property-select/index.html');
+
+        await click(page, 'sap.m.Button')
+        await isPresent(page, 'sap.m.Dialog[title="Test"]')
+
+        let button = await page.$('tag=button');
+        expect(button).to.be.a("object")
+    });
+    
     afterEach(async () => {
+        await browser.close()
+    })
+    
+    afterAll(async () => {
         return new Promise((resolve)=>{
             server.close(() => {
-                console.log('We closed!');
+                console.log('Test http server closed');
                 resolve()
             })
         })
-    })
+    });
 });
